0c22ce5abf4a0dc98c030dace6dd2d0c3f31b108
dasaji-timia <jhoel.salinas@timia.ai>
2025-03-12 23:46:34 -0500

    init aplication

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const MESSAGE = process.env.HELLO_MESSAGE || 'Hello World';
+
+app.get('/', (req, res) => {
+    res.send(MESSAGE);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
